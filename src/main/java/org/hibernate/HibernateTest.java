f2255fd6733fd931343d5639ee13f86979b520f1
LinnykOleh <linnik.oleg.93@gmail.com>
2017-08-19 11:13:04 +0300

    added some fixes, code becomes runnable

--- src/main/java/org/hibernate/HibernateTest.java
+++ src/main/java/org/hibernate/HibernateTest.java
@@ -1,19 +1,20 @@
-import org.hibernate.*;
+package org.hibernate;
+
 import org.hibernate.cfg.Configuration;
+import org.hibernate.dto.UserDetails;
 
 public class HibernateTest {
 
     public static void main(String[] args) {
-        UserDetails user = new UserDetails();
+        final UserDetails user = new UserDetails();
         user.setUserId(1);
         user.setUserName("First User");
 
-        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-        Session session = new sessionFactory.openSession();
+        final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        final Session session = sessionFactory.openSession();
         session.beginTransaction();
         session.save(user);
         session.getTransaction().commit();
-
     }
 
 }
--- src/main/java/org/hibernate/dto/UserDetails.java
+++ src/main/java/org/hibernate/dto/UserDetails.java
@@ -1,3 +1,5 @@
+package org.hibernate.dto;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
