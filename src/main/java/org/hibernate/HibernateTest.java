5852f9d4eb3b075e0de8c89d6f7d698a6e1b4941
maks-1987 <max.moderation@gmail.com>
2017-08-19 15:23:18 +0300

    unit-2.2

--- src/main/java/org/hibernate/HibernateTest.java
+++ src/main/java/org/hibernate/HibernateTest.java
@@ -3,12 +3,17 @@ package org.hibernate;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.dto.UserDetails;
 
+import java.util.Date;
+
 public class HibernateTest {
 
     public static void main(String[] args) {
         final UserDetails user = new UserDetails();
         user.setUserId(1);
         user.setUserName("First User");
+        user.setAddress("First User's Address");
+        user.setJoinedDate(new Date());
+        user.setDescription("Description");
 
         final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         final Session session = sessionFactory.openSession();
--- src/main/java/org/hibernate/dto/UserDetails.java
+++ src/main/java/org/hibernate/dto/UserDetails.java
@@ -1,14 +1,47 @@
 package org.hibernate.dto;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+@Table (name = "USER_DETAILS")
 public class UserDetails {
     @Id
     private int userId;
-    private String userName;
 
+    private String userName;
+    @Temporal(TemporalType.DATE)
+    private Date joinedDate;
+    private String Address;
+    @Lob
+    private String description;
+
+    public Date getJoinedDate() {
+        return joinedDate;
+    }
+
+    public void setJoinedDate(Date joinedDate) {
+        this.joinedDate = joinedDate;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+//    @Id
+//    @Column (name = "USER_ID")
     public int getUserId() {
         return userId;
     }
@@ -16,9 +49,9 @@ public class UserDetails {
     public void setUserId(int userId) {
         this.userId = userId;
     }
-
+//    @Column (name = "USER_NAME")
     public String getUserName() {
-        return userName;
+        return userName + " from getter";
     }
 
     public void setUserName(String userName) {
