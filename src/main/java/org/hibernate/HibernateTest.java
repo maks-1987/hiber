22d4f36337e5419c9236365df012965e627b78c6
maks-1987 <max.moderation@gmail.com>
2017-08-20 12:05:05 +0300

    unit-2.5

--- src/main/java/org/hibernate/HibernateTest.java
+++ src/main/java/org/hibernate/HibernateTest.java
@@ -1,25 +1,42 @@
 package org.hibernate;
-
-import org.hibernate.cfg.Configuration;
 import org.hibernate.dto.UserDetails;
 
+import org.hibernate.cfg.Configuration;
+import org.hibernate.dto.Address;
+import org.hibernate.dto.UserDetails;
 import java.util.Date;
 
 public class HibernateTest {
-
     public static void main(String[] args) {
-        final UserDetails user = new UserDetails();
-        user.setUserId(1);
+        //final
+        UserDetails user = new UserDetails();
         user.setUserName("First User");
-        user.setAddress("First User's Address");
-        user.setJoinedDate(new Date());
-        user.setDescription("Description");
 
-        final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-        final Session session = sessionFactory.openSession();
+        //UserDetails user2 = new UserDetails();
+        //user.setUserName("Second User");
+
+        Address addr = new Address();
+        addr.setStreet("Street Name");
+        addr.setCity("City Name");
+        user.setAddress(addr);
+        //user2.setAddress();
+//        user.setAddress("First User's Address");
+//        user.setJoinedDate(new Date());
+//        user.setDescription("Description");
+        //final
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        //final
+        Session session = sessionFactory.openSession();
         session.beginTransaction();
         session.save(user);
+        //session.save(user2);
         session.getTransaction().commit();
+        session.close();
+//        user = null;
+//        //final
+//        session = sessionFactory.openSession();
+//        session.beginTransaction();
+//        user = (UserDetails) session.get(UserDetails.class, 1);
+//        System.out.println("User name retrieved is " + user.getUserName());
     }
-
 }
--- src/main/java/org/hibernate/dto/UserDetails.java
+++ src/main/java/org/hibernate/dto/UserDetails.java
@@ -1,59 +1,59 @@
 package org.hibernate.dto;
 
 import javax.persistence.*;
-import java.util.Date;
 
 @Entity
 @Table (name = "USER_DETAILS")
 public class UserDetails {
-    @Id
+    @Id @GeneratedValue(strategy=GenerationType.AUTO)
     private int userId;
-
     private String userName;
-    @Temporal(TemporalType.DATE)
-    private Date joinedDate;
-    private String Address;
-    @Lob
-    private String description;
+    @Embedded
+    private Address address;
 
-    public Date getJoinedDate() {
-        return joinedDate;
+    public Address getAddress() {
+        return address;
     }
-
-    public void setJoinedDate(Date joinedDate) {
-        this.joinedDate = joinedDate;
+    public void setAddress(Address address) {
+        this.address = address;
     }
-
-    public String getAddress() {
-        return Address;
-    }
-
-    public void setAddress(String address) {
-        Address = address;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
+    //    @Temporal(TemporalType.DATE)
+//    private Date joinedDate;
+//    private String Address;
+//    @Lob
+//    private String description;
+//
+//    public Date getJoinedDate() {
+//        return joinedDate;
+//    }
+//    public void setJoinedDate(Date joinedDate) {
+//        this.joinedDate = joinedDate;
+//    }
+//    public String getAddress() {
+//        return Address;
+//    }
+//    public void setAddress(String address) {
+//        Address = address;
+//    }
+//    public String getDescription() {
+//        return description;
+//    }
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
 //    @Id
 //    @Column (name = "USER_ID")
     public int getUserId() {
         return userId;
     }
-
     public void setUserId(int userId) {
         this.userId = userId;
     }
 //    @Column (name = "USER_NAME")
-    public String getUserName() {
-        return userName + " from getter";
-    }
 
+    public String getUserName() {
+        return userName;
+    }
     public void setUserName(String userName) {
         this.userName = userName;
     }
